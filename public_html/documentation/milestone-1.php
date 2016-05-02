804f5af6ff47c4e998e57a7ea02eaade7a2dce6e
Jordan Vinson <jvinson3@cnm.edu>
2016-05-02 13:23:12 -0600

    Remade php file and respository, beginning complete recode

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Personal Site</title>
+	</head>
+	<body>
+			<main>
+				<h1>Purpose</h1>
+				<h2>What would you like your PWP to help you accomplish? This is a high-level goal akin to a Mission Statement.</h2>
+				<p>Overall, considering my PWP is a portfolio for me, I'm hoping it provides me with an easy way to show my accomplishments during Deep Dive and, eventually, help me earn a job.</p>
+				<h1>Audience</h1>
+				<h2>Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of? Could it be for your professional peers and other developers, potential employers, customers, or clients, etc.?</h2>
+				<p>I'm hoping that the main people viewing my portfolio would be potential employers and eventually clients, however it is made so anyone cal see it. Anyone that wants to hear about me can view it.
+				<h1>Goal</h1>
+				<p>The goal for my PWP would be to showcase my work in order to give potential clients examples of what future work could look like, as well as to help me acquire a job.
+				<h1>Persona</h1>
+				<p>Ed Barker, 35 year old owner of a Santa Fe startup that sells art made from recycled objects. Wants an online store site to sell his products and ship them around the world to buyers, as well as other website functions such as a donations page, information about the company, and a review page so people can comment on the art. Ed wants a web developer like me to create this site for him. Persona might be facing problems in the form of funding, depending on how much of his art he is able to sell out of his tiny shop in one town. Therefore, site may have a relitively tight budget.</p>
+				<h1>Use Case</h1>
+				<p>Ed would be using my site to learn information about me, as he is interested in hiring. Used my site on the go from a smart phone after attending a presentation where Deep Dive was mentioned.
+			</main>
+	</body>
+</html>
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="eng">
+	<head>
+		<meta charset="UTF-8">
+		<title>Personal Site Planning - Milestone 2</title>
+	</head>
+	<body>
+		<main>
+			<h1>Sitemap</h1>
+			<h2>Pages I plan on including</h2>
+			<h3>Resume Page: My work experience and extra accomplishments</h3>
+			<h3>Personal Page: My interests and hobbies</h3>
+			<h3>Goal Page: My goals as a web developer</h3>
+		</main>
+	</body>
+</html>
