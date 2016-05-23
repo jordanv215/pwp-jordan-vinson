493cf063be946333a016556bec1b047b3db4173b
Jordan Vinson <jvinson3@cnm.edu>
2016-05-23 16:19:07 -0600

    Added a title to the main page of my PWP

---
+++ personal-website-beta.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<link type="text/css" href="css/style.css" rel="sylesheet" />
+
+		<title>Moparvinson.com home site</title>
+	</head>
+</html>
+
